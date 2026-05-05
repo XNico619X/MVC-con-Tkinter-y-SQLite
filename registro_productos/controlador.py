07e432cb4b38bc73d2856a5dbf26d6f3224c3a21
Nico <inge.nicoespi@gmail.com>
2026-05-05 13:06:21 -0500

    arreglo de codigo

--- registro_productos/controlador.py
+++ registro_productos/controlador.py
@@ -8,12 +8,12 @@ class ProductoControlador:
 
     def ejecutar(self):
         while True:
-            opcion = self.vista.mostrar_menu(self.vista)
+            opcion = self.vista.mostrar_menu()
             
             if opcion == '1':
-                nombre, cantidad, precio = self.vista.agregar_producto(self.vista)
+                nombre, cantidad, precio = self.vista.agregar_producto()
                 self.modelo.agregar_producto(nombre, cantidad, precio)
-                self.vista.mostrar_mensaje(self.vista, "¡Producto agregado con éxito!")
+                self.vista.mostrar_mensaje("¡Producto agregado con éxito!")
             
             elif opcion == '2':
                 productos = self.modelo.listar()
@@ -21,15 +21,15 @@ class ProductoControlador:
                     {'id': p[0], 'nombre': p[1], 'cantidad': p[2], 'precio': p[3]} 
                     for p in productos
                 ]
-                self.vista.mostrar_productos(self.vista, productos_formateados)
+                self.vista.mostrar_productos(productos_formateados)
             
             elif opcion == '3':
-                id_prod = self.vista.eliminar_producto(self.vista)
+                id_prod = self.vista.eliminar_producto()
                 self.modelo.eliminar_producto(id_prod)
-                self.vista.mostrar_mensaje(self.vista, f"Producto con ID {id_prod} eliminado.")
+                self.vista.mostrar_mensaje(f"Producto con ID {id_prod} eliminado.")
             
             elif opcion == '4':
-                self.vista.mostrar_mensaje(self.vista, "Saliendo del sistema...")
+                self.vista.mostrar_mensaje("Saliendo del sistema...")
                 break
             else:
-                self.vista.mostrar_mensaje(self.vista, "Opción no válida, intente de nuevo.")
+                self.vista.mostrar_mensaje("Opción no válida, intente de nuevo.")
